74d20136c96cc14ca5177ae23863c100cc879d31
Christian Yotov <christian.yotov@protonmail.com>
2025-07-03 17:54:01 +0300

    Nearly reverting everything from previous commit

---
+++ CMakeOptions.cmake
@@ -0,0 +1,3 @@
+option(IMGUI_GLFW "Compile ImGui with GLFW window creation lib." OFF)
+option(IMGUI_OPENGL "Compile ImGui with opengl graphic API." OFF)
+option(IMGUI_VULKAN "Compile ImGui with Vulkan graphic API" OFF)
